8cf8c56f0663f6c89c191f1f6a2922a235616c09
Arthur PREVOST <arprevost@asi.fr>
2020-09-16 11:49:09 +0200

    First Glance

---
+++ src/Civ.java
@@ -0,0 +1,4 @@
+public class Civ {
+    public String civName;
+
+}
---
+++ src/civStats.java
@@ -0,0 +1,13 @@
+public class civStats extends Civ{
+    public int cultureScore;
+    public int simScore;
+    public int warScore;
+    public int navalScore;
+
+    public civStats (int cS, int sS, int wS, int nS) {
+        cultureScore = cS;
+        simScore = sS;
+        warScore = wS;
+        navalScore = nS;
+    }
+}
