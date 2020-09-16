b9428b8e3eeea2a29925787d9d3e8448a79589db
Arthur PREVOST <arprevost@asi.fr>
2020-09-16 14:12:22 +0200

    WIP

--- src/civStats.java
+++ src/civStats.java
@@ -1,13 +1,45 @@
 public class civStats extends Civ{
-    public int cultureScore;
-    public int simScore;
-    public int warScore;
-    public int navalScore;
+    private int cultureScore;
+    private int simScore;
+    private int warScore;
+    private int navalScore;
 
-    public civStats (int cS, int sS, int wS, int nS) {
-        cultureScore = cS;
+    public civStats (int cultureScore, int sS, int wS, int nS) {
+        this.cultureScore = cultureScore;
         simScore = sS;
         warScore = wS;
         navalScore = nS;
+    }
+
+    public int getCultureScore() {
+        return cultureScore;
+    }
+
+    public void setCultureScore(int cultureScore) {
+        this.cultureScore = cultureScore;
+    }
+
+    public int getSimScore() {
+        return simScore;
+    }
+
+    public void setSimScore(int simScore) {
+        this.simScore = simScore;
+    }
+
+    public int getWarScore() {
+        return warScore;
+    }
+
+    public void setWarScore(int warScore) {
+        this.warScore = warScore;
+    }
+
+    public int getNavalScore() {
+        return navalScore;
+    }
+
+    public void setNavalScore(int navalScore) {
+        this.navalScore = navalScore;
     }
 }
